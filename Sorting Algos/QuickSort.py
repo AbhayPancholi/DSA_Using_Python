dae54613f865c57e1a523a7196c809b865beb79e
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-01 07:22:06 +0530

    Added file for quick sort

--- Sorting Algos/QuickSort.py
+++ Sorting Algos/QuickSort.py
@@ -1,13 +1,13 @@
-def quicksort(arr, left, right):
+def quick_sort(arr, left, right):
     if left < right:
         partition_pos = partition(arr, left, right)
-        quicksort(arr, left, partition_pos - 1)
-        quicksort(arr, partition_pos + 1, right)
+        quick_sort(arr, left, partition_pos - 1)
+        quick_sort(arr, partition_pos + 1, right)
 
 
 def partition(arr, left, right):
     i = left
-    j = right - 1
+    j = right
     pivot = arr[right]
 
     while i < j:
@@ -15,16 +15,14 @@ def partition(arr, left, right):
             i += 1
         while j > left and arr[j] >= pivot:
             j -= 1
-
         if i < j:
             arr[i], arr[j] = arr[j], arr[i]
-
     if arr[i] > pivot:
         arr[i], arr[right] = arr[right], arr[i]
 
     return i
 
 
-arr1 = [2, 6, 4, 9, 1, 0]
-quicksort(arr1, 0, len(arr1) - 1)
+arr1 = [10, 5, 3, 6, 15, 2]
+quick_sort(arr1, 0, len(arr1) - 1)
 print(arr1)
