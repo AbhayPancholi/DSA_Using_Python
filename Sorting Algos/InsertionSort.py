d4bb9798fb690cd1dbec793cac9c3fa9af569b61
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-01 07:20:56 +0530

    Added file for insertion sort

--- Sorting Algos/InsertionSort.py
+++ Sorting Algos/InsertionSort.py
@@ -4,16 +4,11 @@
 # Function to sort array using insertion sort
 def insertionSort(arr):
     for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
+        j = i
 
-        # Move elements of arr[0..i-1], that are
-        # greater than key, to one position ahead
-        # of their current position
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
+        while arr[j - 1] > arr[j] and j > 0:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
             j -= 1
-        arr[j + 1] = key
 
 
 # A utility function to print array of size n
@@ -28,5 +23,3 @@ if __name__ == "__main__":
     arr = [12, 11, 13, 5, 6]
     insertionSort(arr)
     printArray(arr)
-
-    # This code is contributed by Hritik Shah.
