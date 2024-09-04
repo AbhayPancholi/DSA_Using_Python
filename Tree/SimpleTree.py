ff484d0ab1d6271a1a3be49c6976fadb930eb5bb
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-04 23:02:10 +0530

    Added a file demonstrating a simple tree

---
+++ Tree/SimpleTree.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self, data, children=[]) -> None:
+        self.data = data
+        self.children = children
+
+    def __str__(self, level=0) -> str:
+        ret = " " * level + str(self.data) + "\n"
+        for child in self.children:
+            ret += child.__str__(level + 1)
+        return ret
+
+    def AddChild(self, TreeNode):
+        self.children.append(TreeNode)
+
+
+tree = TreeNode("Drinks")
+cold = TreeNode("Cold")
+hot = TreeNode("hot")
+tree.AddChild(cold)
+tree.AddChild(hot)
+print(tree)
