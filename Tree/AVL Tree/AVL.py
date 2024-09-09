ad24a55a8fe883736b680ecf4ae1a16310924e78
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-09 23:21:53 +0530

    Added level order traversal method.

--- Tree/AVL Tree/AVL.py
+++ Tree/AVL Tree/AVL.py
@@ -4,3 +4,19 @@ class AVLNode:
         self.leftChild = None
         self.rightChild = None
         self.height = 1
+
+
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            print(root.value.data)
+
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
