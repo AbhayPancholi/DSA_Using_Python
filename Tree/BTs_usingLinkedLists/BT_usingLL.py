d0d575e154c8e1978cb1b48087bfdf4dd29e251b
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-20 23:18:11 +0530

    Added functions to insert a node and delete a node.

--- Tree/BTs_usingLinkedLists/BT_usingLL.py
+++ Tree/BTs_usingLinkedLists/BT_usingLL.py
@@ -27,3 +27,66 @@ def InsertNode(rootNode, newNode):
             else:
                 root.value.rightChild = newNode
                 return "Successfully inserted"
+
+
+def FindDeepestNode(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+
+        deepestNode = root.value
+        return deepestNode
+
+
+def deleteDeepestNode(rootNode, dNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            if root.value == dNode:
+                root.value = None
+                return
+            if root.value.leftChild:
+                if root.value.leftChild == dNode:
+                    root.value.leftChild = None
+                    return
+                else:
+                    customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild:
+                if root.value.rightChild == dNode:
+                    root.value.rightChild = None
+                    return
+                else:
+                    customQueue.enqueue(root.value.rightChild)
+
+
+def deleteNode(rootNode, node):
+    if not rootNode:
+        return "The binary tree does not exist"
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not customQueue.isEmpty():
+            root = customQueue.dequeue()
+            if root.value.data == node:
+                dNode = FindDeepestNode(rootNode)
+                root.value.data = dNode.data
+                deleteDeepestNode(rootNode, dNode)
+                return "The node has been successfully deleted"
+
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+        return "Failed to delete the node"
