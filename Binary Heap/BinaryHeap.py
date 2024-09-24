0b9ee36d41b8ed2a21b84584fbe1995a26a0c65d
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-24 16:49:40 +0530

    Added levelOrder traversal method.

--- Binary Heap/BinaryHeap.py
+++ Binary Heap/BinaryHeap.py
@@ -19,5 +19,13 @@ def HeapSize(rootNode):
         return rootNode.heapSize
 
 
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        for i in range(1, rootNode.heapSize + 1):
+            print(rootNode.customList[i])
+
+
 newBinaryHeap = Heap(5)
 print(HeapSize(newBinaryHeap))
