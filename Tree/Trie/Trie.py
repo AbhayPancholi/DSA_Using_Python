d133def108a2c4c893617b644cc111503f81a097
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-25 22:43:42 +0530

    Added insert string method

--- Tree/Trie/Trie.py
+++ Tree/Trie/Trie.py
@@ -8,5 +8,19 @@ class Trie:
     def __init__(self) -> None:
         self.root = TrieNode()
 
+    def insertString(self, word):
+        current = self.root
+        for i in word:
+            ch = i
+            node = current.childern.get(ch)
+            if node == None:
+                node = TrieNode()
+                current.childern.update({ch: node})
+            current = node
+        current.endOfString = True
+        print("successfully inserted")
+
 
 newTrie = Trie()
+newTrie.insertString("APP")
+newTrie.insertString("APPL")
