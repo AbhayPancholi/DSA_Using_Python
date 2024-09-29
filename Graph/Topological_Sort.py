e6ea85b3d42d0a4bbf3b34794c497ec82dda4415
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-09-29 16:10:58 +0530

    added code for topological sort method

--- Graph/Topological_Sort.py
+++ Graph/Topological_Sort.py
@@ -8,3 +8,35 @@ class Graph:
 
     def addEdge(self, vertex, edge):
         self.graph[vertex].append(edge)
+
+    def topologicalSortUtil(self, v, visited, stack):
+        visited.append(v)
+
+        for vertex in self.graph[v]:
+            if vertex not in visited:
+                self.topologicalSortUtil(vertex, visited, stack)
+
+        stack.insert(0, v)
+
+    def topologicalSort(self):
+        visited = []
+        stack = []
+
+        for k in list(self.graph):
+            if k not in visited:
+                self.topologicalSortUtil(k, visited, stack)
+        print(stack)
+
+
+custGraph = Graph(8)
+
+custGraph.addEdge("A", "C")
+custGraph.addEdge("C", "E")
+custGraph.addEdge("E", "H")
+custGraph.addEdge("E", "F")
+custGraph.addEdge("F", "G")
+custGraph.addEdge("B", "D")
+custGraph.addEdge("B", "C")
+custGraph.addEdge("D", "F")
+
+custGraph.topologicalSort()
