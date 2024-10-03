1038aed6f6183411ddd29311f8c82ca75e0b886b
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-03 22:53:59 +0530

    Added node class to the file.

--- Graph/Dijkstra's_algo.py
+++ Graph/Dijkstra's_algo.py
@@ -4,3 +4,17 @@ class Egde:
         self.weight = weight
         self.start_vertex = start_vertex
         self.end_vertex = end_vertex
+
+
+# class for nodes
+class Node:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.visited = None
+        # previous node that we come to this node
+        self.predecessor = None
+        self.neighbours = []
+        self.min_distance = float("inf")
+
+    def __lt__(self, other_node):
+        return self.min_distance < other_node.min_distance
