f28eba593a1d634b5f7b7fc562119afb491723f0
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-03 18:09:09 +0530

    Added Dijkstra's algorithm file, rn contains code for edge class only

---
+++ Graph/Dijkstra's_algo.py
@@ -0,0 +1,6 @@
+# class for edges
+class Egde:
+    def __init__(self, weight, start_vertex, end_vertex) -> None:
+        self.weight = weight
+        self.start_vertex = start_vertex
+        self.end_vertex = end_vertex
