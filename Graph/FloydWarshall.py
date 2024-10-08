e0a834f9bfa7073da504e372502e4d4b4af938a6
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-09 01:24:23 +0530

    Added floydWarshall method.

--- Graph/FloydWarshall.py
+++ Graph/FloydWarshall.py
@@ -9,3 +9,13 @@ def print_solution(nV, distance):
             else:
                 print(distance[i][j], end=" ")
         print(" ")
+
+
+def floydWarshall(nV, G):
+    distance = G
+    for k in range(nV):
+        for i in range(nV):
+            for j in range(nV):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+
+    print_solution(nV, distance)
