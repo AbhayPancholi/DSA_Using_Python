ed777f6fa85e63d1a3e524efd03cfed80fcc6513
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-09 01:27:44 +0530

    successfully tested floyd warshall algo.

--- Graph/FloydWarshall.py
+++ Graph/FloydWarshall.py
@@ -5,10 +5,10 @@ def print_solution(nV, distance):
     for i in range(nV):
         for j in range(nV):
             if distance[i][j] == "INF":
-                print("INF", end=" ")
+                print("INF", end="  ")
             else:
-                print(distance[i][j], end=" ")
-        print(" ")
+                print(distance[i][j], end="  ")
+        print("  ")
 
 
 def floydWarshall(nV, G):
@@ -19,3 +19,8 @@ def floydWarshall(nV, G):
                 distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
 
     print_solution(nV, distance)
+
+
+G = [[0, 8, INF, 1], [INF, 0, 1, INF], [4, INF, 0, INF], [INF, 2, 9, 1]]
+
+floydWarshall(4, G)
