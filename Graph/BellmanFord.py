8eaee54955e59a961afdb271e9fb59b0df834166
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-08 23:12:31 +0530

    Added bellman_ford method.

--- Graph/BellmanFord.py
+++ Graph/BellmanFord.py
@@ -14,3 +14,19 @@ class Graph:
         print("Vertex distance form source")
         for key, val in dist.items():
             print("  " + key, " :  " + val)
+
+    def bellman_ford(self, src):
+        dist = {i: float("inf") for i in self.nodes}
+        dist[src] = 0
+
+        for i in range(self.V - 1):
+            for s, d, w in self.graph:
+                if dist[s] != float("inf") and dist[d] > dist[s] + w:
+                    dist[d] = dist[s] + w
+
+        for s, d, w in self.graph:
+            if dist[s] != float("inf") and dist[d] > dist[s] + w:
+                print("Graph contains negative cycle")
+                return
+
+        self.print_solution(dist)
