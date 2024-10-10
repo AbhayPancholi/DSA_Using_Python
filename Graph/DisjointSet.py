5ba36fcf960325e1e72b2c64a16f2af28b248398
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-10 16:27:51 +0530

    Added union function

--- Graph/DisjointSet.py
+++ Graph/DisjointSet.py
@@ -12,3 +12,15 @@ class DisjointSet:
             return item
         else:
             return self.find(self.parent[item])
+
+    def union(self, x, y):
+        xroot = self.find(x)
+        yroot = self.find(y)
+
+        if self.rank[xroot] < self.rank[yroot]:
+            self.parent[xroot] = yroot
+        elif self.rank[yroot] < self.rank[xroot]:
+            self.parent[yroot] = xroot
+        else:
+            self.parent[yroot] = xroot
+            self.rank[xroot] += 1
