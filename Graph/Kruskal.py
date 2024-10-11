452bf5915d68a93258de399926d391dee27b3532
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-12 00:29:08 +0530

    Added kruskal algo function.

--- Graph/Kruskal.py
+++ Graph/Kruskal.py
@@ -1,3 +1,6 @@
+import DisjointSet as dst
+
+
 class Graph:
     def __init__(self, vertices) -> None:
         self.V = vertices
@@ -14,3 +17,18 @@ class Graph:
     def printSolution(self, s, d, w):
         for s, d, w in self.MST:
             print("%s - %s: %s" % (s, d, w))
+
+    def kruskalAlgo(self):
+        i, e = 0, 0
+        ds = dst.DisjointSet(self.nodes)
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        while e < self.V - 1:
+            s, d, w = self.graph[i]
+            i += 1
+            x = ds.find(s)
+            y = ds.find(d)
+            if x != y:
+                e += 1
+                self.MST.append([s, d, w])
+                ds.union(x, y)
+        self.printSolution(s, d, w)
