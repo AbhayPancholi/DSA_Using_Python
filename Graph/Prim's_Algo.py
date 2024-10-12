b46a17595b4c263fc3d04ff99a089c158b8ae59c
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-12 23:58:09 +0530

    successfully tested prim's algorithm

--- Graph/Prim's_Algo.py
+++ Graph/Prim's_Algo.py
@@ -24,10 +24,24 @@ class Graph:
                 if visited[i]:
                     for j in range(self.VertexNum):
                         if (not visited[j]) and self.edges[i][j]:
-                            if min > self.edges[i[j]]:
+                            if min > self.edges[i][j]:
                                 min = self.edges[i][j]
                                 s = i
                                 d = j
-            self.MST.append(self.node[s], self.node[d], self.edges[s][d])
+            self.MST.append([self.nodes[s], self.nodes[d], self.edges[s][d]])
             visited[d] = True
             edgeNum += 1
+        self.printSolution()
+
+
+edges = [
+    [0, 10, 20, 0, 0],
+    [10, 0, 30, 5, 0],
+    [20, 30, 0, 15, 6],
+    [0, 5, 15, 0, 8],
+    [0, 0, 6, 8, 0],
+]
+
+nodes = ["A", "B", "C", "D", "E"]
+g = Graph(nodes, edges, 5)
+g.primsAlgo()
