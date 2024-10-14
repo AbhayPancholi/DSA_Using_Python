d7c523e476eb59402f2536c648a72c5219261f83
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-14 22:11:44 +0530

    Added file and also defined problem and approach to solve it.

---
+++ Greedy Algos/Activity_selection_problem.py
@@ -0,0 +1,9 @@
+# Problem Definition: -
+# In this problem we have to find the maximum number of activities that
+# can be perfomed by a user. We are provided with the start time (at index 1)
+# and end time (at index 2).
+
+# Approach to solve the problem: -
+# To solve this problem we see if the end time of the previous activity is less
+# than the start time of the current activity. If the condition satisfies we will
+# perform the activity else we will go for the next activity
