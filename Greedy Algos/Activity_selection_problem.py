5a86ca3fcde48b29252d8a4afe3ddbec4e27c195
Abhay Pancholi <abhaynagendrapancholi@gmail.com>
2024-10-14 22:33:10 +0530

    Added code for the actvitiy selection problem.

--- Greedy Algos/Activity_selection_problem.py
+++ Greedy Algos/Activity_selection_problem.py
@@ -4,6 +4,32 @@
 # and end time (at index 2).
 
 # Approach to solve the problem: -
-# To solve this problem we see if the end time of the previous activity is less
+# To solve this problem we will first sort the activities based on the end time
+# then we will see if the end time of the previous activity is less
 # than the start time of the current activity. If the condition satisfies we will
 # perform the activity else we will go for the next activity
+
+
+activities = [
+    ["A1", 0, 6],
+    ["A2", 3, 4],
+    ["A3", 1, 2],
+    ["A4", 5, 8],
+    ["A5", 5, 7],
+    ["A6", 8, 9],
+]
+
+
+def printMaxActivities(activities):
+    i = 0
+    activities.sort(key=lambda x: x[2])
+    firstA = activities[i][0]
+    print(firstA)
+
+    for j in range(len(activities)):
+        if activities[j][1] > activities[i][2]:
+            print(activities[j][0])
+            i = j
+
+
+printMaxActivities(activities)
